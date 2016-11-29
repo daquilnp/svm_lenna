3900158d67a12886e7c44ce32fcddaeec11bf924
Nicholas D'Aquila <daquilnp@mcmaster.ca>
2016-11-29 17:38:46 -0500

    added some extra functions for testing best methods and producing figures for the report.

--- edge_detection.py
+++ edge_detection.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('Lenna_img.png',0)
-edges = cv2.Canny(img,100,200)
+edges = cv2.Canny(img,100,100)
 inverted_edges = np.invert(edges)
 cv2.imwrite('edge.png',inverted_edges)
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
